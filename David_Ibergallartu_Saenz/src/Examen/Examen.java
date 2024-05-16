edb1db641d7354d1bfdd216e5f25048ee844f79e
Alumno <Alumno@AULA1-PC06>
2024-05-16 16:33:30 +0200

    Prueba de examen.

---
+++ David_Ibergallartu_Saenz/src/Examen/Examen.java
@@ -0,0 +1,16 @@
+package Examen;
+
+public class Examen {
+
+	public Examen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Hola Nombre, perteneces a la generación del futuro.
+		
+	}
+
+}
---
+++ David_Ibergallartu_Saenz/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module David_Ibergallartu_Saenz {
+}
